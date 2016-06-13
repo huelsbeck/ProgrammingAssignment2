6d9effea131b633e255af6fe64f2605256b9ee03
David P Huelsbeck <david.huelsbeck@gmail.com>
2016-06-12 19:27:56 -0700

    ## Put comments here that give an overall description of what your ## functions do
    ## 'makeCacheMatrix' takes what is presumed to be a square, invetable matrix
    ## and returns a list, the first element of which 'm' is the matrix itself.
    ## The remaining elements of the list are functions for manipulating the
    ## inverse of the matrix, which are used by the companion function 'cacheSolve'
    
    makeCacheMatrix <- function(x = matrix()) {
      i <- NULL
      id <- NULL
      m <- x
    
      geti <- function() i
      seti <- function(inv) i <<- inv
      getid <- function() id
      setid <- function(dd) id <<- dd
    
      return(list(m = m, geti = geti, seti = seti, getid = getid, setid = setid))
    }
    
    ## 'cacheSolve' is the companion function to 'makeCacheMatrix'. It accepts the
    ## list output from 'makeCacheMatrix' and returns the inverse, presuming that
    ## the inverse is defined. If 'cacheSolve' is called more than once for a given
    ## matrix, the stored inverse will be returned provided that the elements of
    ## the matrix have not been altered since the previous call to 'cacheSolve'.
    
    cacheSolve <- function(x, ...) {
            ## Return a matrix that is the inverse of 'x'
      i <- x$geti()
      if(!is.null(i) && identical(x$m,x$getid())) {
        message("returning cached inverse")
        return(i)
      }
      x$setid(x$m)
      i <- x$seti(solve(x$m))
      return(i)
    }

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' takes what is presumed to be a square, invetable matrix
+## and returns a list, the first element of which 'm' is the matrix itself.
+## The remaining elements of the list are functions for manipulating the 
+## inverse of the matrix, which are used by the companion function 'cacheSolve'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  id <- NULL
+  m <- x
+  
+  geti <- function() i
+  seti <- function(inv) i <<- inv
+  getid <- function() id
+  setid <- function(dd) id <<- dd
+  
+  return(list(m = m, geti = geti, seti = seti, getid = getid, setid = setid))
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' is the companion function to 'makeCacheMatrix'. It accepts the
+## list output from 'makeCacheMatrix' and returns the inverse, presuming that
+## the inverse is defined. If 'cacheSolve' is called more than once for a given
+## matrix, the stored inverse will be returned provided that the elements of 
+## the matrix have not been altered since the previous call to 'cacheSolve'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$geti()
+  if(!is.null(i) && identical(x$m,x$getid())) {
+    message("returning cached inverse")
+    return(i)
+  }
+  x$setid(x$m)
+  i <- x$seti(solve(x$m))
+  return(i)
 }
